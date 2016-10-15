e95e04de12e4c7b4a6b7baa375f726b9121ac853
Jonathan Schwob <jonathanrschwob@gmail.com>
2016-10-15 16:01:57 -0700

    rna fin

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,7 +1,20 @@
 var DnaTranscriber = function() {};
 
-DnaTranscriber.prototype.toRna = function (a, b) {
- test;
-}
+DnaTranscriber.prototype.toRna = function (a) {
+	var newRNA = '';
+
+	 for (var i = 0; i < a.length; i ++) {
+		if (a[i] === 'G') {
+			newRNA += 'C';
+		} else if (a[i] === 'C') {
+			newRNA += 'G';
+		} else if (a[i] === 'T') {
+			newRNA += 'A';
+		} else if (a[i] === 'A') {
+			newRNA += 'U'
+		}
+	};
+	return newRNA;
+};
 
 module.exports = DnaTranscriber;
