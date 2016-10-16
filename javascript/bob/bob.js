1359ea71584959a89231527d1af368c1f2360e8a
Jonathan Schwob <jonathanrschwob@gmail.com>
2016-10-16 01:30:27 -0700

    finished bob

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -6,9 +6,15 @@
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+	if(input === input.toUpperCase() && input.toLowerCase() !== input) {
+		return 'Whoa, chill out!';
+	} else if (input[input.length -1] === '?') {
+		return 'Sure.';
+	} else if (input.trim() === '') {
+		return 'Fine. Be that way!';
+	} else {
+		return 'Whatever.';
+	}
 };
 
 module.exports = Bob;
